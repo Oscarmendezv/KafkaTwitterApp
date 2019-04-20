185dffb303866b78d8fafbdee9fe22233ef9bc67
Óscar Méndez <oscar.mendez-valladares@akka.eu>
2019-04-20 17:07:54 +0200

    Changes to project structure

--- KafkaTwitterApp/src/main/java/omendezv/Streams/KafkaTwitterAppApplication.java
+++ KafkaTwitterApp/src/main/java/omendezv/Streams/KafkaTwitterAppApplication.java
@@ -1,4 +1,4 @@
-package omendezv.github.com.KafkaTwitterApp;
+package omendezv.Streams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- KafkaTwitterApp/src/test/java/omendezv/Streams/KafkaTwitterAppApplicationTests.java
+++ KafkaTwitterApp/src/test/java/omendezv/Streams/KafkaTwitterAppApplicationTests.java
@@ -1,4 +1,4 @@
-package omendezv.github.com.KafkaTwitterApp;
+package omendezv.Streams;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
