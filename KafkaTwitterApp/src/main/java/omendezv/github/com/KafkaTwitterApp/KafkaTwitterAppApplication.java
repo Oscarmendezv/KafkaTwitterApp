359fbc1d6761c303c36d62b8ca0d49d0baa72d0d
Óscar Méndez <oscar.mendez-valladares@akka.eu>
2019-04-20 16:57:34 +0200

    Adding the newly generated java project

---
+++ KafkaTwitterApp/src/main/java/omendezv/github/com/KafkaTwitterApp/KafkaTwitterAppApplication.java
@@ -0,0 +1,13 @@
+package omendezv.github.com.KafkaTwitterApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTwitterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaTwitterAppApplication.class, args);
+	}
+
+}
